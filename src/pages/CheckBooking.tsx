f6e70c3912dc22e580a3c05b4296f58cda68bf01
irfanarrosid <irfanarrosid4@gmail.com>
2024-12-24 09:17:19 +0700

    init: install react vite and react-router-dom

---
+++ src/pages/CheckBooking.tsx
@@ -0,0 +1,3 @@
+export default function CheckBooking() {
+  return <p>Check Booking Page</p>;
+}
---
+++ src/pages/SuccessBooking.tsx
@@ -0,0 +1,3 @@
+export default function SuccessBooking() {
+  return <p>Success Booking Page</p>;
+}
